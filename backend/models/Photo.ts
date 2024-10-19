292d509604e683adcdb8fe4ac6160d3c00dcff13
ramilka <lilya1976.03.03@gmail.com>
2024-10-19 11:59:48 +0600

    Сделал модель для фотографий.

---
+++ backend/models/Photo.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const PhotoSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    isPublished: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+});
+
+const Photo = mongoose.model('Photo', PhotoSchema);
+
+export default Photo;
